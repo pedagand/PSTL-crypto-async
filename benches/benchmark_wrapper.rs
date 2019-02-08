9106ba57ad5081d3705c67f951a3e9ec3b89ae84
Yohanan <harrochy96@gmail.com>
2019-02-08 14:23:53 +0100

    Add the benchmark for the wrapper

---
+++ benches/benchmark_wrapper.rs
@@ -0,0 +1,24 @@
+extern crate pstl;
+extern crate criterion;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn wrapper_des_benchmark(c: &mut Criterion) {
+    let mut plain: [u64; 64] = [0x0000000000000001; 64];
+    for i in 0..64 {
+        plain[i as usize] = 13 * i;
+    }
+
+    let mut key: [u64; 56] = [0x0000000000000; 56];
+    for i in 0..56 {
+        key[i as usize] = 7 * i;
+    }
+
+    let  cipher: [u64; 64] = [0x000000000000000; 64];
+
+    c.bench_function("Bench the wrapper des function", move |b| b.iter(|| crate::pstl::wrapper_des::des(plain, key, cipher)));
+}
+
+criterion_group!(benches, wrapper_des_benchmark);
+
+criterion_main!(benches);
