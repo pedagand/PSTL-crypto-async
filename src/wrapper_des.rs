360e6c756a75739ad0e336116c00c88d3d7b6adb
GUO NING <ai535463858@gmail.com>
2019-02-10 10:49:24 +0100

    add test wrapper

--- src/wrapper_des.rs
+++ src/wrapper_des.rs
@@ -1,9 +1,9 @@
 extern "C" {
-    fn DES(plain: &[u64; 64], key: &[u64; 56], cipher: &[u64; 64]);
+    fn DES(plain: &[u64; 64], key: &[u64; 56], cipher: &mut [u64; 64]);
 }
 
-pub fn des(plain: [u64; 64], key: [u64; 56], cipher: [u64; 64]) {
+pub fn des(plain: &[u64; 64], key: &[u64; 56], cipher: &mut [u64; 64]) {
     unsafe {
-        DES(&plain, &key, &cipher);
+        DES(plain, key, cipher);
     }
 }
