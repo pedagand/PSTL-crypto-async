b2cfc79c807c26673f8af00990ed6d9c699680ba
Haryo <32711260+HarrYoha@users.noreply.github.com>
2019-02-12 10:32:59 +0100

    Add a multithreaded "echo" web server (#25)
    Fix #11

---
+++ sandbox/web_server/src/bin/main.rs
@@ -0,0 +1,24 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use web_server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]).to_ascii_lowercase());
+
+}
