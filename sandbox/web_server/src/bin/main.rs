011685bf41737c78e9ffd145a509ac2ce5fddc8e
Yohanan <harrochy96@gmail.com>
2019-03-22 17:40:05 +0100

    fix the error with assert

--- sandbox/web_server/src/bin/main.rs
+++ sandbox/web_server/src/bin/main.rs
@@ -16,9 +16,15 @@ fn main() {
     let vec: Vec<Cell> = vec![Cell { plain: 0, key: 0 }; size];
     let vec: Arc<Mutex<Vec<Cell>>> = Arc::new(Mutex::new(vec));
 
-    let counter = Arc::new(Mutex::new(1));
+    let counter = Arc::new(Mutex::new(0));
+    let counter_write = Arc::new(Mutex::new(0));
     let pool = ThreadPool::new(size);
     let (sender, receiver) = mpsc::channel();
+    let (s, r) = mpsc::channel();
+    let r = Arc::new(Mutex::new(r));
+    let s = Arc::new(Mutex::new(s.clone()));
+
+
     let receiver = Arc::new(Mutex::new(receiver));
     let sender = Arc::new(Mutex::new(sender.clone()));
 
@@ -26,33 +32,48 @@ fn main() {
         let stream = stream.unwrap();
         let sender = Arc::clone(&sender);
         let receiver = Arc::clone(&receiver);
+        let r = Arc::clone(&r);
+        let s = Arc::clone(&s);
         let counter = Arc::clone(&counter);
         let buffer = Arc::clone(&vec);
+        let counter_write = Arc::clone(&counter_write);
 
         pool.execute(move || {
-            handle_connection(stream, counter, buffer, sender, receiver, size);
+            handle_connection(stream, counter, buffer, sender, receiver, size, counter_write, s, r);
         });
     }
 }
 
 
-pub fn handle_connection(mut stream: TcpStream, counter: Arc<Mutex<i32>>, buf: Arc<Mutex<Vec<Cell>>>, sen: Arc<Mutex<mpsc::Sender<()>>>, rec: Arc<Mutex<mpsc::Receiver<()>>>, size: usize) {
+pub fn handle_connection(mut stream: TcpStream, counter: Arc<Mutex<i32>>, buf: Arc<Mutex<Vec<Cell>>>,
+                         sen: Arc<Mutex<mpsc::Sender<()>>>, rec: Arc<Mutex<mpsc::Receiver<()>>>,
+                         size: usize, counter_write: Arc<Mutex<i32>>, s: Arc<Mutex<mpsc::Sender<()>>>,
+                         r: Arc<Mutex<mpsc::Receiver<()>>>) {
     let mut buffer = [0; 8];
     stream.read(&mut buffer).unwrap();
     let plain = u64::from_be_bytes(buffer);
     let key = rand::thread_rng().gen();
+
+
     let mut cpt = counter.lock().unwrap();
-    let number = *cpt;
-    assert!(number >= 1 && number <= size as i32);
+    if *cpt == -1 {
+        let _received = r.lock().unwrap().recv().unwrap();
+        *cpt = 0;
+    }
+    let index = *cpt;
+    //std::mem::drop(cpt);
+
+    assert!(index >= 0 && index <= size as i32);
     let mut buff = buf.lock().unwrap();
-    let index = number - 1;
+
     buff[index as usize].key = key;
     buff[index as usize].plain = plain;
     std::mem::drop(buff);
+    //let mut cpt = counter.lock().unwrap();
     *cpt += 1;
     std::mem::drop(cpt);
 
-    if number == size as i32 {
+    if index == (size as i32 - 1) {
         let mut buff = buf.lock().unwrap();
         for i in 0..(size) {
             buff[i].plain ^= buff[i].key;
@@ -60,20 +81,24 @@ pub fn handle_connection(mut stream: TcpStream, counter: Arc<Mutex<i32>>, buf: A
         }
         assert!(buff[size - 1].plain == key ^ plain);
         stream.write(&u64_to_array_of_u8(buff[size - 1].plain)).unwrap();
-        std::mem::drop(buff);
         let mut cpt = counter.lock().unwrap();
-        *cpt = 1;
+        *cpt = -1;
         for _ in 0..(size - 1) {
             let _sender = sen.lock().unwrap().send(()).unwrap();
         }
+        std::mem::drop(buff);
     } else {
         let _received = rec.lock().unwrap().recv().unwrap();
         let buff = buf.lock().unwrap();
-        let index = number - 1;
         let result = buff[index as usize].plain;
         assert!(result == key ^ plain);
         stream.write(&u64_to_array_of_u8(result)).unwrap();
-        std::mem::drop(buff);
+        let mut c = counter_write.lock().unwrap();
+        *c += 1;
+        if *c == (size as i32) - 1 {
+            let _sender = s.lock().unwrap().send(()).unwrap();
+            *c = 0;
+        }
     }
 }
 
