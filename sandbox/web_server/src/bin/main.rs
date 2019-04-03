4535b6d97153b40e05deb229b4672588fcb82edf
Yohanan <harrochy96@gmail.com>
2019-04-03 19:05:54 +0200

    a possible fix

--- sandbox/web_server/src/bin/main.rs
+++ sandbox/web_server/src/bin/main.rs
@@ -25,6 +25,12 @@ fn main() {
     let s = Arc::new(Mutex::new(s.clone()));
 
 
+    let (sd, rd) = mpsc::channel();
+    let rd = Arc::new(Mutex::new(rd));
+    let sd = Arc::new(Mutex::new(sd.clone()));
+    let counter_wait = Arc::new(Mutex::new(0));
+
+
     let receiver = Arc::new(Mutex::new(receiver));
     let sender = Arc::new(Mutex::new(sender.clone()));
 
@@ -34,12 +40,18 @@ fn main() {
         let receiver = Arc::clone(&receiver);
         let r = Arc::clone(&r);
         let s = Arc::clone(&s);
+
+        let rd = Arc::clone(&rd);
+        let sd = Arc::clone(&sd);
+        let counter_wait = Arc::clone(&counter_wait);
+
+
         let counter = Arc::clone(&counter);
         let buffer = Arc::clone(&vec);
         let counter_write = Arc::clone(&counter_write);
 
         pool.execute(move || {
-            handle_connection(stream, counter, buffer, sender, receiver, size, counter_write, s, r);
+            handle_connection(stream, counter, buffer, sender, receiver, size, counter_write, s, r, sd, rd, counter_wait);
         });
     }
 }
@@ -48,56 +60,67 @@ fn main() {
 pub fn handle_connection(mut stream: TcpStream, counter: Arc<Mutex<i32>>, buf: Arc<Mutex<Vec<Cell>>>,
                          sen: Arc<Mutex<mpsc::Sender<()>>>, rec: Arc<Mutex<mpsc::Receiver<()>>>,
                          size: usize, counter_write: Arc<Mutex<i32>>, s: Arc<Mutex<mpsc::Sender<()>>>,
-                         r: Arc<Mutex<mpsc::Receiver<()>>>) {
+                         r: Arc<Mutex<mpsc::Receiver<()>>>, sd: Arc<Mutex<mpsc::Sender<()>>>,
+                         rd: Arc<Mutex<mpsc::Receiver<()>>>, counter_wait: Arc<Mutex<i32>>) {
     let mut buffer = [0; 8];
     stream.read(&mut buffer).unwrap();
     let plain = u64::from_be_bytes(buffer);
     let key = rand::thread_rng().gen();
 
 
-    let mut cpt = counter.lock().unwrap();
-    if *cpt == -1 {
+    let mut counter_thread = counter.lock().unwrap();
+    if *counter_thread == -1 {
         let _received = r.lock().unwrap().recv().unwrap();
-        *cpt = 0;
+        *counter_thread = 0;
     }
-    let index = *cpt;
-    //std::mem::drop(cpt);
-
+    let index = *counter_thread;
+    *counter_thread += 1;
+    std::mem::drop(counter_thread);
     assert!(index >= 0 && index <= size as i32);
-    let mut buff = buf.lock().unwrap();
 
+    let mut buff = buf.lock().unwrap();
     buff[index as usize].key = key;
     buff[index as usize].plain = plain;
     std::mem::drop(buff);
-    //let mut cpt = counter.lock().unwrap();
-    *cpt += 1;
-    std::mem::drop(cpt);
 
-    if index == (size as i32 - 1) {
+
+    if index != (size as i32 - 1) {
+        let mut c_wait = counter_wait.lock().unwrap();
+        *c_wait += 1;
+        if *c_wait == (size as i32) - 1 {
+            sd.lock().unwrap().send(()).unwrap();
+            *c_wait = 0;
+        }
+        std::mem::drop(c_wait);
+        let _received = rec.lock().unwrap().recv().unwrap();
+        let buff = buf.lock().unwrap();
+        let result = buff[index as usize].plain;
+        std::mem::drop(buff);
+        assert!(result == key ^ plain);
+        stream.write(&u64_to_array_of_u8(result)).unwrap();
+        let mut c_write = counter_write.lock().unwrap();
+        *c_write += 1;
+        if *c_write == (size as i32) - 1 {
+            s.lock().unwrap().send(()).unwrap();
+            *c_write = 0;
+        }
+    } else {
+        rd.lock().unwrap().recv().unwrap();
         let mut buff = buf.lock().unwrap();
         for i in 0..(size) {
-            buff[i].plain ^= buff[i].key;
+            buff[i].plain = buff[i].plain ^ buff[i].key;
             thread::sleep(time::Duration::from_millis(1));
         }
         assert!(buff[size - 1].plain == key ^ plain);
         stream.write(&u64_to_array_of_u8(buff[size - 1].plain)).unwrap();
-        let mut cpt = counter.lock().unwrap();
-        *cpt = -1;
-        for _ in 0..(size - 1) {
-            let _sender = sen.lock().unwrap().send(()).unwrap();
-        }
         std::mem::drop(buff);
-    } else {
-        let _received = rec.lock().unwrap().recv().unwrap();
-        let buff = buf.lock().unwrap();
-        let result = buff[index as usize].plain;
-        assert!(result == key ^ plain);
-        stream.write(&u64_to_array_of_u8(result)).unwrap();
-        let mut c = counter_write.lock().unwrap();
-        *c += 1;
-        if *c == (size as i32) - 1 {
-            let _sender = s.lock().unwrap().send(()).unwrap();
-            *c = 0;
+
+        let mut counter_thread = counter.lock().unwrap();
+        *counter_thread = -1;
+        std::mem::drop(counter_thread);
+
+        for _ in 0..(size - 1) {
+            sen.lock().unwrap().send(()).unwrap();
         }
     }
 }
